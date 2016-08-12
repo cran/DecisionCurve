5205e7ff5f7d0c5b72db3c5357805868b2bf0345
Marshall Brown <mdbrown@fredhutch.org>
2016-08-12 08:58:01 +0000

    version 1.3

--- R/plot_functions_sub.R
+++ R/plot_functions_sub.R
@@ -31,7 +31,14 @@ preparePlotData   <- function(x, curve.names, confidence.intervals){
     }
 
     if(is.na(confidence.intervals[1])){
-      confidence.intervals <- x[[1]]$confidence.intervals
+
+      ci.list <- sapply(x, FUN = function(x) x$confidence.intervals)
+      ci.list.log <- sapply(x, FUN = function(x) is.numeric(x$confidence.intervals))
+      ci.list.num <- as.numeric(ci.list[ci.list.log])
+
+      if(length(unique(ci.list.num))!=1){warning("Confidence intervals of different sizes are being plotted on the same figure.")}
+
+      confidence.intervals <- ifelse(any(ci.list.log), 1, "none")
     }else{
       confidence.intervals <- ifelse(confidence.intervals, 1, "none")
     }
@@ -209,11 +216,11 @@ plot_generic<- function(xx, predictors, value, plotNew,
   if(is.element(legend.position, c("bottomright", "topright", "bottomleft", "topleft", "right", "left", "top", "bottom"))){
 
     if(value == "NB" | value == "sNB"){
-     legend(legend.position, lty = lty, col = col, lwd = lwd, legend = c(predictors, "All", "None"))
+     legend(legend.position, lty = lty, col = col, lwd = lwd, legend = c(predictors, "All", "None"),  bg  = "white")
     }else if(tpr.fpr.legend){
       n.preds <- length(predictors)
       legend(legend.position,
-             lty = c( lty.tpr, lty.fpr),
+             lty = c( lty.tpr, lty.fpr), bg  = "white",
              col = col,
              lwd = lwd, legend = c("True positive rate", "False positive rate"))
 
@@ -221,7 +228,7 @@ plot_generic<- function(xx, predictors, value, plotNew,
     } else if(impact.legend){
       legend(legend.position,
              lty = c( 1, 2),
-             col = col,
+             col = col, bg  = "white",
              lwd = lwd, legend = c("Number high risk", "Number high risk with outcome"))
 
     }
